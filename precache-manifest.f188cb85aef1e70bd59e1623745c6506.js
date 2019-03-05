bac99f0c7e53d0db221770bbf85e8d25fc9c477d
Hoon-MacBookPro <hoon-macbookpro@Hoon-MacBookProui-MacBookPro.local>
2019-03-05 20:35:03 +0900

    Updates

---
+++ precache-manifest.f188cb85aef1e70bd59e1623745c6506.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd285588f7040d3e798b",
+    "url": "/contactmanager/static/css/main.03cd0a58.chunk.css"
+  },
+  {
+    "revision": "fd285588f7040d3e798b",
+    "url": "/contactmanager/static/js/main.fd285588.chunk.js"
+  },
+  {
+    "revision": "e4bf40f27ed8dbd6f9da",
+    "url": "/contactmanager/static/js/runtime~main.e4bf40f2.js"
+  },
+  {
+    "revision": "b766f0d546331a15e41c",
+    "url": "/contactmanager/static/css/2.711f52f8.chunk.css"
+  },
+  {
+    "revision": "b766f0d546331a15e41c",
+    "url": "/contactmanager/static/js/2.b766f0d5.chunk.js"
+  },
+  {
+    "revision": "76daf4c6674e4b889e56e3f518f58689",
+    "url": "/contactmanager/index.html"
+  }
+];
